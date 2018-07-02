6afb700f7690bd78eda7ef9025aeb0fddc18e4e7
Daniel Fernández Marqués <daniFM333@gmail.com>
2018-07-02 10:22:13 +0200

    Tank Building 03
    Wheels

--- code/Tank.cpp
+++ code/Tank.cpp
@@ -34,32 +34,40 @@ namespace bullet_3da
 
 		// wheel r1
 		shared_ptr<Rigid_Body> wheel_rb_r1(new Dynamic_Rigid_Body(wheel_shape, 10.f));
-		wheel_rb_r1->get()->getWorldTransform().setOrigin(btVector3(position.x - 7.f, position.y - 8.8f, position.z - 3.f));
+		wheel_rb_r1->get()->getWorldTransform().setOrigin(btVector3(position.x - 7.f, position.y - 3.f, position.z + 9));
 		wheel_rb_r1->get()->setRestitution(0.f);
 		wheel_rb_r1->get()->btCollisionObject::setActivationState(DISABLE_DEACTIVATION);
 		shared_ptr<Entity> wheel_r1(new Entity(/*scene, */"../../assets/Wheel.obj", wheel_rb_r1));
 
 		// wheel r2
 		shared_ptr<Rigid_Body> wheel_rb_r2(new Dynamic_Rigid_Body(wheel_shape, 10.f));
-		wheel_rb_r2->get()->getWorldTransform().setOrigin(btVector3(position.x - 7.f, position.y - 3.f, position.z - 16.f));
+		wheel_rb_r2->get()->getWorldTransform().setOrigin(btVector3(position.x - 7.f, position.y - 3.f, position.z - 9.5f));
 		wheel_rb_r2->get()->setRestitution(0.f);
 		wheel_rb_r2->get()->btCollisionObject::setActivationState(DISABLE_DEACTIVATION);
 		shared_ptr<Entity> wheel_r2(new Entity(/*scene, */"../../assets/Wheel.obj", wheel_rb_r2));
 
 		// wheel l1
 		shared_ptr<Rigid_Body> wheel_rb_l1(new Dynamic_Rigid_Body(wheel_shape, 10.f));
-		wheel_rb_l1->get()->getWorldTransform().setOrigin(btVector3(position.x + 7.f, position.y - 8.8f, position.z - 3.f));
+		wheel_rb_l1->get()->getWorldTransform().setOrigin(btVector3(position.x + 7.f, position.y - 3.f, position.z + 9));
 		wheel_rb_l1->get()->setRestitution(0.f);
 		wheel_rb_l1->get()->btCollisionObject::setActivationState(DISABLE_DEACTIVATION);
 		shared_ptr<Entity> wheel_l1(new Entity(/*scene, */"../../assets/Wheel.obj", wheel_rb_l1));
+
+		// wheel l2
+		shared_ptr<Rigid_Body> wheel_rb_l2(new Dynamic_Rigid_Body(wheel_shape, 10.f));
+		wheel_rb_l2->get()->getWorldTransform().setOrigin(btVector3(position.x + 7.f, position.y - 3.f, position.z - 9.5f));
+		wheel_rb_l2->get()->setRestitution(0.f);
+		wheel_rb_l2->get()->btCollisionObject::setActivationState(DISABLE_DEACTIVATION);
+		shared_ptr<Entity> wheel_l2(new Entity(/*scene, */"../../assets/Wheel.obj", wheel_rb_l2));
 
 		
 
 		scene->add("body", body);
 		scene->add("turret", turret);
 		scene->add("wheel_r1", wheel_r1);
-		//scene->add("wheel_r2", wheel_r2);
-		//scene->add("wheel_l1", wheel_l1);
+		scene->add("wheel_r2", wheel_r2);
+		scene->add("wheel_l1", wheel_l1);
+		scene->add("wheel_l2", wheel_l2);
 
 		//add_constraints(scene, turret_constraint, *turret_rb->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(0.f, 0.0f, 0.f), btVector3(0.f, -1.f, 0.f), btVector3(0.f, 0.f, 0.f), true);
 
@@ -68,17 +76,21 @@ namespace bullet_3da
 		scene->get_physics_world()->addConstraints(turret_constraint);
 
 
-		wheel_r1_constraint = new btHingeConstraint(*wheel_rb_r1->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(-7.f, -8.8f, -3.f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
+		wheel_r1_constraint = new btHingeConstraint(*wheel_rb_r1->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(-7.f, -3.f, 9.f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
 		wheel_r1_constraint->enableAngularMotor(true, 0.f, 10.f);
 		scene->get_physics_world()->addConstraints(wheel_r1_constraint);
 
-		/*wheel_r2_constraint = new btHingeConstraint(*wheel_rb_r2->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(-7.f, -9.5f, -3.f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
+		wheel_r2_constraint = new btHingeConstraint(*wheel_rb_r2->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(-7.f, -3.f, -9.5f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
 		wheel_r2_constraint->enableAngularMotor(true, 0.f, 10.f);
-		scene->get_physics_world()->addConstraints(wheel_r2_constraint);*/
+		scene->get_physics_world()->addConstraints(wheel_r2_constraint);
 
-		/*wheel_l1_constraint = new btHingeConstraint(*wheel_rb_l1->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(7.f, -3.f, -3.f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
+		wheel_l1_constraint = new btHingeConstraint(*wheel_rb_l1->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(7.f, -3.f, 9.f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
 		wheel_l1_constraint->enableAngularMotor(true, 0.f, 10.f);
-		scene->get_physics_world()->addConstraints(wheel_l1_constraint);*/
+		scene->get_physics_world()->addConstraints(wheel_l1_constraint);
+
+		wheel_l2_constraint = new btHingeConstraint(*wheel_rb_l2->get(), *body_rb->get(), btVector3(0.f, 0.f, 0.f), btVector3(7.f, -3.f, -9.5f), btVector3(0.f, 0.f, -1.f), btVector3(0.f, 0.f, 0.f));
+		wheel_l2_constraint->enableAngularMotor(true, 0.f, 10.f);
+		scene->get_physics_world()->addConstraints(wheel_l2_constraint);
 		
 	}
 }
